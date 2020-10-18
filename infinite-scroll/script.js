4e1486e5ffd071a7963a470f048bb125d24dabe6
rcruz <rubenamcruz@gmail.com>
2020-10-18 16:28:31 +0100

    infinite loading of images

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -2,6 +2,10 @@ const spinner = document.getElementById('spinner');
 const apiUrl = "https://api.unsplash.com/";
 const headers = {"Authorization": "Client-ID hkyyTkyzmSESSYel3hlBCKQbqgP4LdK255Xg9szNkVA"};
 const image_container = document.getElementById('image-container');
+const number_of_images_to_load = 15;
+
+let canLoadMorePictures = true;
+let numberOfLoadedImages = 0;
 
 /**
  * Spinner Controll
@@ -15,6 +19,19 @@ function doneLoadingPictures(){
     spinner.style.visibility = "hidden";
 }
 
+/**
+ * Image loaded count
+ */
+function imageLoaded(){
+    console.log("loaded!");
+    if(++numberOfLoadedImages == number_of_images_to_load){
+        canLoadMorePictures = true;
+        numberOfLoadedImages = 0;
+        console.log("ready");
+    }
+
+}
+
 
 //////////////////////////////////
 
@@ -25,6 +42,7 @@ function addImageToDocument(image){
 
     let image_elem = document.createElement('img');
     image_elem.setAttribute("src", image.urls.raw);
+    image_elem.onload = imageLoaded();
     if(image.description != null){
         image_elem.setAttribute("title", image.description);
     }
@@ -34,6 +52,7 @@ function addImageToDocument(image){
 
 
 async function loadMorePictures(numberOfPicturesToLoad){
+    canLoadMorePictures = false;
     loadingMorePictures();
     
     const response = await fetch(`${apiUrl}/photos/random?count=${numberOfPicturesToLoad}`, {headers: headers});
@@ -46,4 +65,12 @@ async function loadMorePictures(numberOfPicturesToLoad){
     doneLoadingPictures();
 }
 
-let response = loadMorePictures(5);
+document.body.onscroll = () => {
+    if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight - 1000 && canLoadMorePictures) {
+        // you're at the bottom of the page
+        loadMorePictures(number_of_images_to_load);
+    }
+}
+
+loadMorePictures(number_of_images_to_load);
+
